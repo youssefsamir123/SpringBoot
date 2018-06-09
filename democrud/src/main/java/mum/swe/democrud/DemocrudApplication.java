82950fd55257d87b0fe36ac13f0b720d7ed05805
youssefsamir123 <39744334+youssefsamir123@users.noreply.github.com>
2018-06-09 02:07:53 -0500

    SpringBoot Linked
    SBL

---
+++ democrud/src/main/java/mum/swe/democrud/DemocrudApplication.java
@@ -0,0 +1,18 @@
+package mum.swe.democrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@EnableAutoConfiguration
+@ComponentScan("mum.swe.democrud")
+public class DemocrudApplication {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(DemocrudApplication.class, args);
+	}
+}
